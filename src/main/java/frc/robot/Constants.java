1836a04b78eed2829ddf20b6dd8ac748e571f03e
jagxas <erdem1umut1iel@gmail.com>
2023-01-16 10:21:16 +0300

    init

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,22 @@
+package frc.robot;
+
+public class Constants {
+    public static class ChassisConstants {
+        public final static int idLeftMaster = 51;
+        public final static int idLeftSlave = 52;
+        public final static int idRigthMaster = 50;
+        public final static int idRightSlave =53;
+        public final static int idMidMaster = 0;
+        public final static int idMidSlave = 0;
+
+        public final static double lowerOutput = 0.3;
+        public final static double higherOutput = 0.6;
+
+        public final static int maxVel = 2000;
+        public final static int maxAcc = 1500;
+
+    }
+    public static class ControllerConstants {
+        public final static int turboButton = 6;
+    }
+}
---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,103 @@
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.PS4Controller;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+
+public class Robot extends TimedRobot {
+
+  private CANSparkMax leftMaster = new CANSparkMax(Constants.ChassisConstants.idLeftMaster, MotorType.kBrushless);
+  private CANSparkMax leftSlave = new CANSparkMax(Constants.ChassisConstants.idLeftSlave, MotorType.kBrushless);
+  private CANSparkMax rightMaster = new CANSparkMax(Constants.ChassisConstants.idRigthMaster, MotorType.kBrushless);
+  private CANSparkMax rightSlave = new CANSparkMax(Constants.ChassisConstants.idRightSlave, MotorType.kBrushless);
+  private TalonSRX midMaster = new TalonSRX(Constants.ChassisConstants.idMidMaster);
+  private TalonSRX midSlave = new TalonSRX(Constants.ChassisConstants.idMidSlave);
+
+  private DifferentialDrive drive;
+
+  private SparkMaxPIDController leftpid;
+  private SparkMaxPIDController rightpid;
+
+
+  private PS4Controller driverController = new PS4Controller(0);
+
+
+  @Override
+  public void robotInit() {
+    leftMaster.setInverted(true); // Should I also add follower motors here?
+    rightMaster.setInverted(true);
+    midMaster.setInverted(false);
+
+    leftSlave.follow(leftMaster);
+    rightSlave.follow(rightMaster);
+    midSlave.follow(midMaster);
+
+    leftSlave.close();
+    rightSlave.close();
+
+    leftpid = leftMaster.getPIDController();
+    rightpid = rightMaster.getPIDController();
+
+    leftpid.setSmartMotionMaxVelocity(Constants.ChassisConstants.maxVel, 0);
+    leftpid.setSmartMotionMaxAccel(Constants.ChassisConstants.maxAcc, 0);
+
+    rightpid.setSmartMotionMaxVelocity(Constants.ChassisConstants.maxVel, 0);
+    rightpid.setSmartMotionMaxAccel(Constants.ChassisConstants.maxAcc, 0);
+
+    drive = new DifferentialDrive(leftMaster, rightMaster);
+  }
+
+  @Override
+  public void robotPeriodic() {}
+
+  @Override
+  public void autonomousInit() {}
+
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {}
+
+  public void driveFunctions() {
+
+    drive.setMaxOutput(Constants.ChassisConstants.lowerOutput);
+    if(driverController.getRawButtonPressed(6)){drive.setMaxOutput(Constants.ChassisConstants.higherOutput);}
+    drive.arcadeDrive(-driverController.getLeftX() * 0.5, -driverController.getLeftY());
+
+    midMaster.set(TalonSRXControlMode.PercentOutput, driverController.getRightX());
+
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    leftpid.setReference(0, CANSparkMax.ControlType.kSmartMotion);
+    rightpid.setReference(0, CANSparkMax.ControlType.kSmartMotion);
+    driveFunctions();
+  }
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void testInit() {}
+
+  @Override
+  public void testPeriodic() {}
+
+  @Override
+  public void simulationInit() {}
+
+  @Override
+  public void simulationPeriodic() {}
+}
